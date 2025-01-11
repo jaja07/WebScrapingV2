9456e957a6660ca11eceb45aec8fc4438d7e3c0f
blackdoll12 <florinedjoua@gmail.com>
2025-01-11 17:00:44 +0100

    scraping from nike

---
+++ scraper/items/items/spiders/nikeSpider.py
@@ -0,0 +1,21 @@
+import scrapy
+from ..items import ItemsItem
+
+
+class NikespiderSpider(scrapy.Spider):
+    name = "nikeSpider"
+    allowed_domains = ["nike.com"]
+    start_urls = ["https://www.nike.com/fr/w/hommes-football-maillots-dequipe-1gdj0z3a41eznik1"]
+
+    def parse(self, response):
+        jerseys=response.xpath('//main/section/div/div')
+        for j in jerseys:
+         
+            maillot = ItemsItem()
+            maillot['lien'] = j.xpath('./div/figure/a[@class="product-card__link-overlay"]/@href').get()
+            maillot['nom'] = j.xpath('./div/figure/div/div/div[@class="product-card__titles"]/div[@class="product-card__title"]/text()').get()
+            maillot['prix'] = j.xpath('./div/figure/div/div[@class="product-card__animation_wrapper"]/div/div/div/div[contains(@class, "is--current-price css")]/text()').get()
+            yield maillot
+
+        
+        
