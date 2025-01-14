78a32bfce452801d15cf2fc8da30fe49ec62d89f
kcap02 <kencapochichi@yahoo.com>
2025-01-14 14:44:15 +0100

    Modification du fichier settings

--- scraper/items/items/settings.py
+++ scraper/items/items/settings.py
@@ -65,9 +65,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "items.pipelines.ItemsPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "items.pipelines.ItemsPipeline": 300,
+    "items.pipelines.MongoDBPipeline": 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
